1431d47d25decae31d5d617691eed2977aae1ac5
FranciscoVilca <francs22554@gmail.com>
2025-02-17 13:58:20 -0300

    Inicio proyecto

---
+++ src/main/java/ar/edu/unju/edm/trabajofinalfebreropv/TrabajofinalfebreropvApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm.trabajofinalfebreropv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajofinalfebreropvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajofinalfebreropvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unju/edm/trabajofinalfebreropv/TrabajofinalfebreropvApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm.trabajofinalfebreropv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajofinalfebreropvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
