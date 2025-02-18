ffbd53d47480c7526e7c5875be2a822e1fa95348
FranciscoVilca <130519325+FranciscoVilca@users.noreply.github.com>
2025-02-18 18:35:04 -0300

    Update ConductorRepository.java

--- src/main/java/ar/edu/unju/edm/trabajofinalfebreropv/Repository/ConductorRepository.java
+++ src/main/java/ar/edu/unju/edm/trabajofinalfebreropv/Repository/ConductorRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import ar.edu.unju.edm.trabajofinalfebreropv.model.Conductor;
-ll
+
 @Repository
 public interface ConductorRepository extends JpaRepository<Conductor, Long> {
 
